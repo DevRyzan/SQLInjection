1e699d6901d6aa20c13b204e4d49ed9a5298ee75
RezanSoylemez <rezan.sm@gmail.com>
2024-12-22 18:42:49 +0100

    Fixed - Filepathandinit

--- src/Infrastructure/API/main.py
+++ src/Infrastructure/API/main.py
@@ -1,3 +1,7 @@
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../../..')))
+
 from Infrastructure.API.DbConfig import engine, Base
 from Infrastructure.Application.DbContext import DbContext
 
